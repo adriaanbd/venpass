fe9989fed4fd8a2257dc1dcfdd3e489c9476fe3b
Adriaan Beiertz <adriaanbd@gmail.com>
2019-08-01 18:46:05 -0500

    updated evaluators responsibility to evaluate only

--- evaluator.rb
+++ evaluator.rb
@@ -1,24 +1,25 @@
 class Evaluator
+  attr_accessor :strength
 
-  attr_reader :strength
-  attr_accessor :password
-
-  def initialize(password)
-    @password = password.downcase
+  def initialize
     @strength = nil
   end
 
-  def replace_word(words)
-    nil if words.empty?
-    words.each { |word| @password.gsub!(word, word[0]) }
+  def calculate_strength(words, password)
+    password_to_compare = replace_words(words, password)
+    char_types(password_to_compare) * (password.size - 1)
   end
 
-  def char_types
+  def replace_word(words, password)
+    words.each { |word| password.gsub!(word, word[0]) }
+  end
+
+  def char_types(password)
     alpha = 0
     digit = 0
     whitespace = 0
     other = 0
-    @password.each_char do |char|
+    password.each_char do |char|
       if char.match?(/[a-z]/i)
         alpha += 1 if alpha.zero?
       elsif char.match?(/\d/)
--- spec/evaluator_spec.rb
+++ spec/evaluator_spec.rb
@@ -5,14 +5,9 @@ RSpec.describe Evaluator do
   let(:two_words) { "play31ground" }
   let(:three_types) { "test 31" }
   let(:four_types) { "test 31*" }
-  let(:evaluator) { Evaluator.new(word) }
-  let(:evaluator_2) { Evaluator.new(two_words) }
+  let(:evaluator) { Evaluator.new }
 
   describe '#init' do 
-    it 'initializes password instance attribute' do 
-      expect(evaluator.password).to eq(word)
-    end
-
     it 'initializes strength instance attribute' do 
       expect(evaluator.strength).to eq(nil)
     end
@@ -20,19 +15,19 @@ RSpec.describe Evaluator do
 
   describe '#character_types' do 
     it 'identifies one character type' do 
-      expect(evaluator.char_types).to eq(1)
+      expect(evaluator.char_types(word)).to eq(1)
     end
 
     it 'identifies two character types' do 
-      expect(evaluator_2.char_types).to eq(2)
+      expect(evaluator.char_types(two_words)).to eq(2)
     end
 
     it 'identifies three character types' do 
-      expect(Evaluator.new(three_types).char_types).to eq(3)
+      expect(evaluator.char_types(three_types)).to eq(3)
     end
 
     it 'identifies four character types' do 
-      expect(Evaluator.new(four_types).char_types).to eq(4)
+      expect(evaluator.char_types(four_types)).to eq(4)
     end
   end
 end
