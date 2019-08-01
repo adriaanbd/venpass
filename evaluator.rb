92c68ac727e51abccd006699ef4957aba4d90afb
Adriaan Beiertz <adriaanbd@gmail.com>
2019-07-31 22:53:25 -0500

    added spec and functions to replace_words and get count of unique char_types

--- evaluator.rb
+++ evaluator.rb
@@ -4,10 +4,34 @@ class Evaluator
 
   def initialize(password)
     @password = password
-    @strength = 0
+    @strength = nil
   end
 
   def sequence_of_letters
     @password.scan(/[a-z]+/i)
+  end
+
+  def replace_word(words)
+    nil if words.empty?
+    words.each { |word| @password.gsub!(word, word[0]) }
+  end
+
+  def char_types
+    alpha = 0
+    digit = 0
+    whitespace = 0
+    other = 0
+    @password.each_char do |char|
+      if char.match?(/[a-z]/i)
+        alpha += 1 if alpha.zero?
+      elsif char.match?(/\d/)
+        digit += 1 if digit.zero?
+      elsif char.match?(/\s/)
+        whitespace += 1 if whitespace.zero?
+      else
+        other += 1 if other.zero?
+      end
+    end
+    alpha + digit + whitespace + other
   end
 end
--- spec/evaluator_spec.rb
+++ spec/evaluator_spec.rb
@@ -3,17 +3,18 @@ require './evaluator'
 RSpec.describe Evaluator do
   let(:word) { "playground" }
   let(:two_words) { "play31ground" }
+  let(:three_types) { "test 31" }
+  let(:four_types) { "test 31*" }
   let(:evaluator) { Evaluator.new(word) }
   let(:evaluator_2) { Evaluator.new(two_words) }
 
   describe '#init' do 
     it 'initializes password instance attribute' do 
       expect(evaluator.password).to eq(word)
-      expect(evaluator.strength).to eq(0)
     end
 
     it 'initializes strength instance attribute' do 
-      expect(evaluator.strength).to eq(0)
+      expect(evaluator.strength).to eq(nil)
     end
 
   end
@@ -25,6 +26,32 @@ RSpec.describe Evaluator do
 
     it 'identifies two sequences of letters' do 
       expect(evaluator_2.sequence_of_letters).to eq(['play', 'ground'])
+    end
+  end
+
+  describe '#replace_word' do 
+    it 'replaces two words each with one character' do 
+      words = evaluator_2.sequence_of_letters
+      evaluator_2.replace_word(words)
+      expect(evaluator_2.password).to eq('p31g')
+    end
+  end
+
+  describe '#character_types' do 
+    it 'identifies one character type' do 
+      expect(evaluator.char_types).to eq(1)
+    end
+
+    it 'identifies two character types' do 
+      expect(evaluator_2.char_types).to eq(2)
+    end
+
+    it 'identifies three character types' do 
+      expect(Evaluator.new(three_types).char_types).to eq(3)
+    end
+
+    it 'identifies four character types' do 
+      expect(Evaluator.new(four_types).char_types).to eq(4)
     end
   end
 end
